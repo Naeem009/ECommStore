112d9d701c6cb6f0930d130f4fbe6e60209c1b74
m.n.shahzad <m.n.shahzad@gmail.com>
2024-06-12 08:55:52 +0000

    UPDATES NEX.CONFIG.MJS

--- next.config.mjs
+++ next.config.mjs
@@ -6,3 +6,15 @@ const nextConfig = {
   };
   
   export default nextConfig;
+
+  const path = require('path');
+
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['@styles'] = path.join(__dirname, 'styles');
+    config.resolve.alias['@types'] = path.join(__dirname, 'types');
+    return config;
+  },
+};
