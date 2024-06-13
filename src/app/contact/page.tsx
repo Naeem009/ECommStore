8b46f9208531ec1f57a4514965d631205b02eb17
m.n.shahzad <m.n.shahzad@gmail.com>
2024-06-13 10:24:33 +0000

    updated contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import * as React from "react";
+import { useState } from "react";
 
 export default function Contact(){
-    return(
-        <div>
-            <h1 className="flex w-full item-center justify-center font-bold text-3xl text-neutral-800">Contact</h1>
-        </div>
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [mob, setMob] = useState("");
+        return(
+        <form autoComplete="off" className="items-center justify-center flex flex-col gap-4 mt-10">
+            <label htmlFor="input credentials" className="text-lg font-semibold mb-2 mt-2 text-yellow-700">For contact please enter your details</label>
+            <input className="w-full max-w-xs border-2 border-gray-300 p-2 rounded bg-transparent shadow-lg" type="text" id="name" placeholder="Enter Your Name" value={name} onChange={(e) => setName(e.target.value)}/>
+            <input className="w-full max-w-xs border-2 border-gray-300 p-2 rounded bg-transparent shadow-lg" type="email" id="email" placeholder="Enter Your Email Address" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input className="w-full max-w-xs border-2 border-gray-300 p-2 rounded bg-transparent shadow-lg" type="text" id="mobilenumber" placeholder="Enter Your Mobile Number" value={mob} onChange={(e) => setMob(e.target.value)}/>
+            <button className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded shadow-lg" type="submit">Submit</button>
+        </form>
     );
 };
